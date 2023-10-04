6803306ec31230fe80ced8fa3e197f463ce71308
Hari02052000 <harisankar02052000@gmail.com>
2023-10-04 10:57:14 +0530

    updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
    // baseUrl: 'http://54.79.197.145:3000'
   //  baseUrl: 'http://localhost:3000'
-    baseUrl:'https://auto-space.onrender.com'
+   baseUrl:'https://auto-space.onrender.com'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    //baseUrl: 'http://54.79.197.145:3000'
+   // baseUrl: 'http://54.79.197.145:3000'
    baseUrl:'https://auto-space.onrender.com'
     //baseUrl: 'http://localhost:3000'
   };
